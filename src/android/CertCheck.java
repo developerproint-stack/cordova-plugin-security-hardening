a83991ab98382cf119aea8e946247e444e5a38dd
unknown <developer.proint@gmail.com>
2026-07-22 18:40:55 +0700

    enhance security android v4

--- src/android/CertCheck.java
+++ src/android/CertCheck.java
@@ -5,6 +5,7 @@ import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import android.util.Log;
 
 public class CertCheck extends CordovaPlugin {
 
